92e9a84a653c4acd3217534e30db3362adaeb35b
Shrey Shakya <23shakya@gmail.com>
2026-09-20 15:40:00 +0545

    added playGame function to bundle and clean code

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,60 +1,40 @@
-let humanScore = 0
-let computerScore = 0
+function getHumanChoice(humanChoice) {
+  if (humanChoice === 'rock') {
+    return "You chose rock"
+  } else if (humanChoice === 'paper') {
+    return "You chose paper"
+  } else if (humanChoice === 'scissors') {
+    return "you chose scissors"
+  } else {
+    return "please enter a valid humanChoice"
+  }
+}
 
-for (let i = 0; i < 5; i++) {
-    let humanChoice = String(prompt("Please pick your move: ")).toLowerCase()
-    let computerChoice = Math.floor(Math.random() * 3)
-
-    function getHumanChoice(humanChoice) {
-        if (humanChoice === 'rock') {
-            return "You chose rock"
-        } else if (humanChoice === 'paper') {
-            return "You chose paper"
-        } else if (humanChoice === 'scissors') {
-            return "you chose scissors"
-        } else {
-            return "please enter a valid humanChoice"
-        }
-    }
-
-    function getComputerChoice(computerChoice) {
-        if (computerChoice === 0) {
-            return "Computer chose: rock"
-        } else if (computerChoice === 1) {
-            return "Computer chose: paper"
-        } else if (computerChoice === 2) {
-            return "computer chose: scissors"
-        }
-    }
-    function selectWinner(humanChoice, computerChoice) {
-        if (humanChoice === 'rock' && computerChoice === 2) {
-            humanScore++
-            return "You won"
-        } else if (humanChoice === 'scissors' && computerChoice === 1) {
-            humanScore++
-            return "You won"
-        } else if (humanChoice === 'paper' && computerChoice === 0) {
-            humanScore++
-            return "You Won"
-        } else if (humanChoice === 'rock' && computerChoice === 1) {
-            computerScore++
-            return "You Lose"
-        } else if (humanChoice === 'scissors' && computerChoice === 0) {
-            computerScore++
-            return "You Lose"
-        } else if (humanChoice === 'paper' && computerChoice === 2) {
-            computerScore++
-            return "You Lose"
-        } else {
-            return "Draw"
-        }
-    }
-  
-    console.log(getHumanChoice(humanChoice))
-    console.log(getComputerChoice(computerChoice))
-    console.log(selectWinner(humanChoice, computerChoice))
-    console.log(`Your current score: ${humanScore}`)
-    console.log(`Computer's score: ${computerScore}`)
+function getComputerChoice(computerChoice) {
+  if (computerChoice === 0) {
+    return "Computer chose: rock"
+  } else if (computerChoice === 1) {
+    return "Computer chose: paper"
+  } else if (computerChoice === 2) {
+    return "computer chose: scissors"
+  }
+}
+function selectWinner(humanChoice, computerChoice) {
+  if (humanChoice === 'rock' && computerChoice === 2) {
+    return "You Won"
+  } else if (humanChoice === 'scissors' && computerChoice === 1) {
+    return "You Won"
+  } else if (humanChoice === 'paper' && computerChoice === 0) {
+    return "You Won"
+  } else if (humanChoice === 'rock' && computerChoice === 1) {
+    return "You Lose"
+  } else if (humanChoice === 'scissors' && computerChoice === 0) {
+    return "You Lose"
+  } else if (humanChoice === 'paper' && computerChoice === 2) {
+    return "You Lose"
+  } else {
+    return "Draw"
+  }
 }
 
 function finalScore(humanScore, computerScore) {
@@ -67,5 +47,32 @@ function finalScore(humanScore, computerScore) {
   }
 }
 
-console.log(finalScore(humanScore, computerScore))
+function playGame() {
+  let humanScore = 0
+  let computerScore = 0
 
+  for (let i = 0; i < 5; i++) {
+    let humanChoice = String(prompt("Please pick your move: ")).toLowerCase()
+    let computerChoice = Math.floor(Math.random() * 3)
+
+    console.log(getHumanChoice(humanChoice))
+    console.log(getComputerChoice(computerChoice))
+
+    let result = selectWinner(humanChoice, computerChoice)
+    console.log(result)
+
+    if (result === 'You Won') {
+      humanScore++
+    } else if (result === 'You Lose') {
+      computerScore++
+    }
+
+    console.log(`Your current score: ${humanScore}`)
+    console.log(`Computer's score: ${computerScore}`)
+  }
+
+  console.log(finalScore(humanScore, computerScore))
+
+}
+
+playGame()
